ef2fd37b964f036f9b996672d621a0093a1a2bb0
okikiola11 <okikiola.apelehin@gmail.com>
2021-10-10 17:43:23 +0000

    Install test dependencies Add test to the HomePage.js Component

--- src/__test__/components/Hompage.test/Homepage.test.js
+++ src/__test__/components/Hompage.test/Homepage.test.js
@@ -1,28 +1,48 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import renderer from 'react-test-renderer';
+import { Link } from 'react-router-dom';
+import { shallowRenderWithoutProvider } from '../../setup';
+import HomePage from '../../../components/HomePage/HomePage';
 
-import HomePage from '../../components/Hompage.test';
+describe('Homepage', () => {
+  const page = shallowRenderWithoutProvider(HomePage);
 
-describe('HomePage', () => {
-  it('renders HomePage component with a message', () => {
-    render(
-      <Router>
-        <HomePage />
-      </Router>,
-    );
-    expect(screen.getByText('Symply Book Me')).toBeInTheDocument();
+  test('Should render Homepage', () => {
+    expect(page).toBeTruthy();
+    expect(page).not.toBeFalsy();
+    expect(page).toMatchSnapshot();
   });
 
-  it('matches Homepage snapshot', () => {
-    const tree = renderer
-      .create(
-        <Router>
-          <HomePage />
-        </Router>,
-      )
-      .toJSON();
-    expect(tree).toMatchSnapshot();
+  test('Should render nav', () => {
+    const nav = page.find('nav');
+
+    expect(nav.length).toBe(1);
+    expect(nav.length).not.toBe(0);
+  });
+
+  test('Should render logo', () => {
+    const logo = page.find('.logo');
+
+    expect(logo.length).toBe(1);
+    expect(logo.length).not.toBe(0);
+    expect(logo.text()).not.toBe('');
+    expect(logo.text()).toBe('SYMPLY BOOK ME');
+  });
+
+  test('Should render register and login links in nav', () => {
+    const nav = page.find('nav div');
+    const links = nav.find(Link);
+
+    expect(links.length).toBe(3);
+    expect(nav.length).not.toBe(0);
+    expect(links.first().text()).toBe('SYMPLY BOOK ME');
+    expect(links.last().text()).toBe('Login');
+  });
+
+  test('Should render welcome text', () => {
+    const h1 = page.find('h1');
+
+    expect(h1.length).toBe(1);
+    expect(h1.length).not.toBe(0);
+    expect(h1.text()).toBe('Book a Lawyer for your affairs today');
+    expect(h1.text()).not.toBe('');
   });
 });
---
+++ src/__test__/setup.js
@@ -0,0 +1,72 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+import { fireEvent, render } from '@testing-library/react';
+import thunk from 'redux-thunk';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import Enzyme, { mount, shallow } from 'enzyme';
+import { BrowserRouter } from 'react-router-dom';
+import { act } from 'react-dom/test-utils';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const middleware = [thunk];
+const mockStore = configureMockStore(middleware);
+const store = mockStore({
+  auth: {
+    user: {
+      appointment: [],
+      gym_sessions: [],
+      id: 'id-01',
+      email: 'test@mail.com',
+    },
+  },
+  isLoading: false,
+  error: null,
+});
+
+export const shallowRender = (Element, props = {}) => (
+  shallow(
+    <Provider store={store}>
+      <BrowserRouter>
+        <Element {...props} />
+      </BrowserRouter>
+    </Provider>,
+  )
+);
+
+export const shallowRenderWithoutProvider = (Element, props = {}) => (
+  shallow(
+    <Element {...props} />,
+  )
+);
+
+export const mountRender = (Element, props = {}) => (
+  mount(
+    <Provider store={store}>
+      <BrowserRouter>
+        <Element {...props} />
+      </BrowserRouter>
+    </Provider>,
+  )
+);
+
+export const renderWrapper = (Element, props = {}) => {
+  const { getByLabelText, container } = render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <Element {...props} />
+      </BrowserRouter>
+    </Provider>,
+  );
+  return { getByLabelText, container };
+};
+
+export const changeInputText = async (element, value = '') => {
+  await act(async () => {
+    fireEvent.change(element, { target: { value } });
+    fireEvent.blur(element);
+  });
+};
+
+export const getById = (container, id) => container.querySelector(`#${id}`);
