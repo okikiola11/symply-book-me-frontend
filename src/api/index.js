a910d80403f36e5062847d7e2adfc34b139204d9
okikiola11 <okikiola.apelehin@gmail.com>
2021-10-12 18:56:23 +0000

    Install the axios dependency package Use axios for the RESTFUL actions for users, lawyers, and appointments

--- src/api/index.js
+++ src/api/index.js
@@ -1,116 +1,63 @@
-// const baseUrl = 'http://localhost:3000/api/v1';
-const baseUrl = 'https://glacial-chamber-50989.herokuapp.com/api/v1';
+/* eslint-disable no-param-reassign */
+import axios from 'axios';
+
+const baseURL = 'http://localhost:3000/api/v1';
+// const baseURL = 'https://glacial-chamber-50989.herokuapp.com/api/v1';
 // const baseUrl = 'https://admiring-brown-f9346f.netlify.app/api/v1';
 
+const axiosInstance = axios.create({
+  baseURL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+axiosInstance.interceptors.request.use((config) => {
+  config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
+  return config;
+},
+(error) => Promise.reject(error));
+
 export const userRegistration = async (userObj) => {
-  const createdUser = await fetch(`${baseUrl}/users`, {
-    method: 'POST',
-    mode: 'cors',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-    },
-    body: JSON.stringify(userObj),
-  });
-  const { user, token } = await createdUser.json();
-  console.log(createdUser, 'hello');
+  const { data } = await axiosInstance.post('/users', userObj);
+  const { user, token } = data;
   localStorage.setItem('token', token);
   return user;
 };
 
 export const userLogin = async (userObj) => {
-  const loggedInUser = await fetch(`${baseUrl}/login`, {
-    method: 'POST',
-    mode: 'cors',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(userObj),
-  });
-  const user = await loggedInUser.json();
+  const { data } = await axiosInstance.post('/login', userObj);
+  const { user } = data;
+  // const user = await loggedInUser.json();
   localStorage.setItem('token', user.token);
   return user;
 };
 
 export const signOut = async () => {
-  const logOut = await fetch(`${baseUrl}/logout`, {
-    method: 'DELETE',
-    mode: 'cors',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${localStorage.getItem('token')}`,
-    },
-  });
-  const userLogOut = await logOut.json();
-  return userLogOut;
+  const { data } = await axiosInstance.delete('/logout');
+  return data;
 };
 
 export const fetchAllLawyers = async () => {
-  const fetchedLawyers = await fetch(`${baseUrl}/lawyers`, {
-    mode: 'cors',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${localStorage.getItem('token')}`,
-    },
-  });
-  const allLawyers = await fetchedLawyers.json();
-  return allLawyers;
+  const { data } = await axiosInstance.get('/lawyers');
+  return data;
 };
 
 export const fetchALawyer = async (id, setLawyer) => {
-  const getALawyer = await fetch(`${baseUrl}/lawyers/${id}`, {
-    mode: 'cors',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${localStorage.getItem('token')}`,
-    },
-  });
-  const aLawyer = await getALawyer.json();
-  return setLawyer(aLawyer);
+  const { data } = await axiosInstance.get(`/lawyers/${id}`);
+  return setLawyer(data);
 };
 
-export const bookAppointment = async (data) => {
-  const appointmentBooking = await fetch(`${baseUrl}/appointments`, {
-    method: 'POST',
-    mode: 'cors',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${localStorage.getItem('token')}`,
-    },
-    body: JSON.stringify(data),
-  });
-  const appointmentCall = await appointmentBooking.json();
-  return appointmentCall;
+export const bookAppointment = async (datas) => {
+  const { data } = await axiosInstance.post('appointments', datas);
+  return data;
 };
 
 export const fetchAppointments = async () => {
-  const displayAllAppointment = await fetch(`${baseUrl}/appointments`, {
-    mode: 'cors',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${localStorage.getItem('token')}`,
-    },
-  });
-  const allAppointments = await displayAllAppointment.json();
-  return allAppointments;
+  const { data } = await axiosInstance.get('appointments');
+  return data;
 };
 
 export const deleteAppointments = async (id) => {
-  const deletedAppointed = await fetch(`${baseUrl}/appointments/${id}`, {
-    method: 'DELETE',
-    mode: 'cors',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${localStorage.getItem('token')}`,
-    },
-  });
-  const appointment = await deletedAppointed.json();
-  return appointment;
+  const { data } = await axiosInstance.delete(`appointments/${id}`);
+  return data;
 };
